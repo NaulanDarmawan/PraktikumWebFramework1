1cff59b1c36ed7015004b6a8796d9e854e869fff
Naulan Darmawan <naulandarmawan588@gmail.com>
2025-12-23 12:34:03 +0700

    feat: Menambah Autentikasi Manual Untuk Admin Dan Manager

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -2,34 +2,52 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class UserModel extends Model
+class UserModel extends Authenticatable
 {
     use HasFactory;
 
     protected $table = 'm_user';
     protected $primaryKey = 'user_id';
 
-    // --- HASIL PRAKTIKUM 1 (FILLABLE) ---
     protected $fillable = [
         "level_id",
         "username",
         "nama",
-        "password"
+        "password",
+        "created_at",
+        "updated_at"
     ];
 
-    // protected $fillable = [
-    //     "level_id",
-    //     "username",
-    //     "nama"
-    // ];
+    protected $hidden = [
+        'password'
+    ];
 
-    // --- HASIL PRAKTIKUM 2.7 (RELATIONSHIP) ---
+    protected $casts = [
+        'password' => 'hashed',
+    ];
+
     public function level() : BelongsTo
     {
         return $this->belongsTo(LevelModel::class, 'level_id', 'level_id');
+    }
+
+    public function getRole()
+    {
+        return $this->level->level_kode;
+    }
+
+    public function getRoleName() : string
+    {
+        return $this->level->level_nama;
+    }
+
+    public function hasRole($role) : bool
+    {
+        return $this->level->level_kode === $role;
     }
 }
